f49ecd042cbef5d69664c3e3b3c30ffa16323145
AlexisFetet <fetet.alexis@gmail.com>
2023-02-06 22:50:47 +0100

    port ads msg and move decode to dedicated package

--- ros_ads_msgs_decode/src/ADSDecode.cpp
+++ ros_ads_msgs_decode/src/ADSDecode.cpp
@@ -1,8 +1,8 @@
-#include <ros_ads_msgs/ADSDecode.h>
+#include "ros_ads_msgs/ADSDecode.hpp"
 
 using namespace ros_ads_msgs;
 
-std::map<std::string, variant_t> ros_ads_msgs::decode(const ros_ads_msgs::ADS::ConstPtr &msg)
+std::map<std::string, variant_t> ros_ads_msgs::decode(const ros_ads_msgs::msg::Ads::SharedPtr p_msg)
 {
     std::map<std::string, std::variant<bool, uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, int64_t, float, double, tm>> result;
     std::string name;
@@ -11,88 +11,86 @@ std::map<std::string, variant_t> ros_ads_msgs::decode(const ros_ads_msgs::ADS::C
     bool cresult;
     std::variant<bool, uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, int64_t, float, double, tm> converted_value;
 
-    int minSize = std::min(std::min(msg->varNames.size(), msg->varTypes.size()), msg->varValues.size());
+    int minSize = std::min(std::min(p_msg->var_names.size(), p_msg->var_types.size()), p_msg->var_values.size());
 
-    for(int index = 0; index < minSize; index ++)
+    for (int index = 0; index < minSize; index++)
     {
         cresult = false;
-        name = msg->varNames[index];
-        type = msg->varTypes[index];
-        value = msg->varValues[index];
+        name = p_msg->var_names[index];
+        type = p_msg->var_types[index];
+        value = p_msg->var_values[index];
         do
         {
-            if(type == "BOOL")
+            if (type == "BOOL")
             {
                 converted_value = static_cast<bool>(value);
                 cresult = true;
             }
-            if(type == "BYTE" || type == "USINT")
+            if (type == "BYTE" || type == "USINT")
             {
                 converted_value = static_cast<uint8_t>(value);
                 cresult = true;
             }
-            if(type == "SINT")
+            if (type == "SINT")
             {
                 converted_value = static_cast<int8_t>(value);
                 cresult = true;
             }
-            if(type == "WORD" || type == "UINT")
+            if (type == "WORD" || type == "UINT")
             {
                 converted_value = static_cast<uint16_t>(value);
                 cresult = true;
             }
-            if(type == "INT")
+            if (type == "INT")
             {
                 converted_value = static_cast<int16_t>(value);
                 cresult = true;
             }
-            if(type == "DWORD" || type == "UDINT")
+            if (type == "DWORD" || type == "UDINT")
             {
                 converted_value = static_cast<uint32_t>(value);
                 cresult = true;
             }
-            if(type == "DINT")
+            if (type == "DINT")
             {
                 converted_value = static_cast<int32_t>(value);
                 cresult = true;
                 break;
             }
-            if(type == "LINT")
+            if (type == "LINT")
             {
                 converted_value = static_cast<int64_t>(value);
                 cresult = true;
                 break;
             }
-            if(type == "REAL")
+            if (type == "REAL")
             {
                 converted_value = static_cast<float>(value);
                 cresult = true;
                 break;
             }
-            if(type == "LREAL")
+            if (type == "LREAL")
             {
                 converted_value = static_cast<double>(value);
                 cresult = true;
                 break;
             }
-            if(type == "DATE")
+            if (type == "DATE")
             {
                 auto temp = static_cast<uint32_t>(value);
-                ros::Time currentDate(temp);
-                time_t tDate(currentDate.toSec());
+                rclcpp::Time currentDate(temp);
+                time_t tDate(RCUTILS_NS_TO_S(currentDate.nanoseconds()));
                 tm tmDate;
-                gmtime_r(&tDate,&tmDate);
+                gmtime_r(&tDate, &tmDate);
                 converted_value = tmDate;
                 cresult = true;
                 break;
             }
-        }
-        while(false);
-        if(cresult)
+        } while (false);
+        if (cresult)
         {
             result[name] = converted_value;
         }
     }
     return result;
 }
-
